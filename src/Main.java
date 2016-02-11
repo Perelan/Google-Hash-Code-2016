63e96f1900840b5eef71dbe7d9cd01981b7d0d4e
LitenApe <thanhsonvo95@gmail.com>
2016-02-11 20:52:06 +0100

    Main, almost done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 class Main{
     public static void main(String[]args) throws IOException{
-        Map map;
+        //Map map;
         String[] products;
-        int nrOfWareHouses;
+        int nrOfWareHouses = 0;
+        int rows=0,colums=0,nrOfDrones=0,deadLine=0,maxLoad=0;
         try{
             Scanner fileScanner = new Scanner(new File("redundancy.in"));
             String read;
@@ -14,14 +16,14 @@ class Main{
                 read = fileScanner.nextLine();
                 if(lineNr == 0){
                     String[] strArr = read.split("\\s+");
-                    int rows = Integer.parseInt(strArr[0]);
-                    int colums = Integer.parseInt(strArr[1]);
-                    int nrOfDrones = Integer.parseInt(strArr[2]);
-                    int deadLine = Integer.parseInt(strArr[3]);
-                    int maxLoad = Integer.parseInt(strArr[4]);
+                    rows = Integer.parseInt(strArr[0]);
+                    colums = Integer.parseInt(strArr[1]);
+                    nrOfDrones = Integer.parseInt(strArr[2]);
+                    deadLine = Integer.parseInt(strArr[3]);
+                    maxLoad = Integer.parseInt(strArr[4]);
                     lineNr++;
                 }else if(lineNr == 1){
-                    map.maxWeight(Integer.parseInt(read));
+                    //map.maxWeight(Integer.parseInt(read));
                     lineNr++;
                 }else if(lineNr == 2){
                     products = read.split("\\s+");
@@ -31,12 +33,24 @@ class Main{
                     lineNr++;
                 }else if(lineNr == 4){ //adding warehouses
                     for(int i = 0;i < nrOfWareHouses;i++){
+                        System.out.println(i);
+                        System.out.println("warehous cord: " + read);
                         String[] xy = read.split("\\s+");
                         read = fileScanner.nextLine();
+                        System.out.println("Prods in wareHouse" + read);
+                        String[] productsInWareHouse = read.split("\\s+");
+                        read = fileScanner.nextLine();
                     }
+                    lineNr++;
+                }else{
+                    //System.out.println(read);
                 }
-                System.out.println(read);
             }
+
+            System.out.println("Rows: " + rows);
+            System.out.println("Col: " + colums);
+            System.out.println("Nr of Drones: " + nrOfDrones);
+            System.out.println("Nr of warehouses: " + nrOfWareHouses);
         }catch(Exception e){
             System.out.println("An error occured while reading file");
         }
