0ab78082f2bdd90807460fe48ad656e90a2de579
Perelan <sinriat@gmal.com>
2016-02-11 19:54:40 +0100

    Fxied thread

--- src/Map.java
+++ src/Map.java
@@ -1,18 +1,11 @@
-public class Map extends Thread{
+public class Map{
     private final int GRID_SIZE = 10000;
 
     private int[][] grid;
     private ArrayList<Drone> drone_list;
 
-    public Map(){
-        grid = new int[GRID_SIZE - 1][GRID_SIZE - 1];
-    }
-
-    public void run(){
-        // Execute a command. - In main class call: object.start()
-    }
-
-    public ArrayList<Drone> getList(){
-        return list;
+    public Map(ArrayList<Drone> drone_list){
+        grid            = new int[GRID_SIZE - 1][GRID_SIZE - 1];
+        this.drone_list = drone_list;
     }
 }
