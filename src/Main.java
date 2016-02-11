631edf589a49e5dc06f3c09818a27352cbd545c0
LitenApe <thanhsonvo95@gmail.com>
2016-02-11 20:05:44 +0100

    Changes to map and adding main
    see title

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 class Main{
     public static void main(String[]args) throws IOException{
         Map map;
+        ArrayList<Strings> orders = new ArrayList<>();
         try{
             Scanner fileScanner = new Scanner(new File("redundancy.in"));
             String read;
@@ -17,7 +18,12 @@ class Main{
                     int nrOfDrones = Integer.parseInt(strArr[2]);
                     int deadLine = Integer.parseInt(strArr[3]);
                     int maxLoad = Integer.parseInt(strArr[4]);
-                    
+                    lineNr++;
+                }else if(lineNr == 1){
+                    map.maxWeight(Integer.parseInt(read));
+                    lineNr++;
+                }else{
+
                 }
                 System.out.println(read);
             }
--- src/Map.java
+++ src/Map.java
@@ -1,15 +1,11 @@
-public class Map extends Thread{
+public class Map{
     private final int GRID_SIZE = 10000;
 
     private int[][] grid;
     private ArrayList<Drone> drone_list;
 
-    public Map(){
+    public Map(int row, int col, int nrOfDrones,int deadLine, int maxLoad){
         grid = new int[GRID_SIZE - 1][GRID_SIZE - 1];
-    }
-
-    public void run(){
-        // Execute a command. - In main class call: object.start()
     }
 
     public ArrayList<Drone> getList(){
