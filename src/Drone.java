ae400606f503103b87d82ff941ccbc5d013dfb23
Perelan <sinriat@gmal.com>
2016-02-11 20:10:32 +0100

    drone working

--- src/Drone.java
+++ src/Drone.java
@@ -1,16 +1,44 @@
-class Drone{
-  private int drone_id;
-  private String drone_cmd;
+class Drone extends Thread {
 
-  public int getDrone_ID(){
-    return drone_id;
-  }
+     private int drone_id;
+     private String drone_cmd;
+     private boolean waiting = true;
+     private boolean ready   = false;
 
-  public String getDrone_cmd(){
-    return drone_cmd;
-  }
+     public Drone(int drone_id, String drone_cmd){
+         this.drone_id  = drone_id;
+         this.drone_cmd = drone_cmd;
+     }
 
-  public void set_cmd(String cmd){
-    this.drone_cmd = cmd;
-  }
+     public void run(){
+         System.out.println("Starting: " + Thread.currentThread().getName() + "Drone-id: " + drone_id);
+         synchronized(this){
+            // Execute drone COMMAND here!
+
+
+            notify(); //Notify main when task is completed
+         }
+     }
+
+     public int getDrone_ID(){
+         return drone_id;
+     }
+
+     public String getDrone_cmd(){
+         return drone_cmd;
+     }
+
+     synchronized void startWait() {
+       try {
+          while(!ready) wait();
+       }
+       catch(InterruptedException exc) {
+          System.out.println("wait() interrupted");
+       }
+    }
+
+    synchronized void notice() {
+       ready = true;
+       notify();
+    }
 }
