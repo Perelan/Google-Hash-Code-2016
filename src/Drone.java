83e0b6e95282bcb4998f4d9050ea3010d187a374
Perelan <sinriat@gmal.com>
2016-02-11 21:07:54 +0100

    fixes

--- src/Drone.java
+++ src/Drone.java
@@ -1,23 +1,45 @@
+import java.util.*;
+import java.lang.*;
+
 class Drone extends Thread
 {
      private int drone_id;
      private String drone_cmd;
      private boolean waiting = true;
      private boolean ready   = false;
+     private int row;
+     private int column;
 
-     public Drone(int drone_id, String drone_cmd){
+     public Drone(int drone_id, String drone_cmd, int row, int column){
          this.drone_id  = drone_id;
          this.drone_cmd = drone_cmd;
+         this.row = row;
+         this.column = column;
      }
 
      public void run(){
          System.out.println("Starting: " + Thread.currentThread().getName() + "Drone-id: " + drone_id);
          synchronized(this){
             // Execute drone COMMAND here!
-
-
+            if(drone_cmd.equalsIgnoreCase("load")){
+                load();
+            }else if(drone_cmd.equalsIgnoreCase("deliver")){
+                deliver();
+            }else{
+                System.out.println(String.format("Drone(%d) had an invaild command!", drone_id));
+            }
             notify(); //Notify main when task is completed
          }
+     }
+
+     public void load(){
+        //number of
+        
+
+     }
+
+     public void deliver(){
+
      }
 
      public int getDrone_ID(){
@@ -41,4 +63,8 @@ class Drone extends Thread
        ready = true;
        notify();
     }
+
+     public int find_distance(Warehouse wh){
+         return Math.sqrt(((row - wh.row)*(row - wh.row)) + ((column - wh.column)*(colmn - wh.column)));
+     }
 }
