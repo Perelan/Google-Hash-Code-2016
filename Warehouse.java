3445f6aaae4e13d81ba276fc62ef874a32640aaf
Perelan <sinriat@gmal.com>
2016-02-11 19:08:58 +0100

    fixed warehouse

---
+++ Warehouse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Warehouse {
+
+    private ArrayList<Product> list;
+    private int warehouse_id;
+
+    public Warehouse(int warehouse_id){
+        this.warehouse_id = warehouse_id;
+        list = new ArrayList<>();
+    }
+
+    public void sett_inn(Product p){
+        list.add(p);
+        System.out.println(String.format("Product %s were inserted into %s", p.getProduct_id, warehouse_id));
+    }
+}
