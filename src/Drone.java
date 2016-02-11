2a873380eab4671b5c188ff536085f0d94fc0e62
LitenApe <thanhsonvo95@gmail.com>
2016-02-11 19:55:17 +0100

    test
    test

--- src/Drone.java
+++ src/Drone.java
@@ -1,16 +1,26 @@
-class Drone{
+class Drone extends Thread{
   private int drone_id;
   private String drone_cmd;
+  private Map deliveryMap;
+
+  public Drone(int drone_id,Map delMap){
+      this.drone_id = drone_id;
+      this.deliveryMap = delMap;
+  }
+
+  public void run(){
+      deliveryMap.delivered();
+  }
 
   public int getDrone_ID(){
-    return drone_id;
+      return drone_id;
   }
 
   public String getDrone_cmd(){
-    return drone_cmd;
+      return drone_cmd;
   }
 
   public void set_cmd(String cmd){
-    this.drone_cmd = cmd;
+      this.drone_cmd = cmd;
   }
 }
