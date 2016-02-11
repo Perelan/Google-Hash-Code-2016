7a77839d8f441509085230a013f5f40244d1ac6f
Perelan <sinriat@gmal.com>
2016-02-11 20:12:03 +0100

    fixed map

--- src/Map.java
+++ src/Map.java
@@ -4,17 +4,11 @@ public class Map{
     private int[][] grid;
     private ArrayList<Drone> drone_list;
 
-<<<<<<< HEAD
-    public Map(ArrayList<Drone> drone_list){
-        grid            = new int[GRID_SIZE - 1][GRID_SIZE - 1];
-        this.drone_list = drone_list;
-=======
     public Map(int row, int col, int nrOfDrones,int deadLine, int maxLoad){
         grid = new int[GRID_SIZE - 1][GRID_SIZE - 1];
     }
 
     public ArrayList<Drone> getList(){
         return list;
->>>>>>> 631edf589a49e5dc06f3c09818a27352cbd545c0
     }
 }
