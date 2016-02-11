a987233fa1e82759c93b43b3d15c554be689a548
LitenApe <thanhsonvo95@gmail.com>
2016-02-11 19:14:33 +0100

    added drone
    theirs a drone brat

---
+++ src/Drone.java
@@ -0,0 +1,16 @@
+class Drone{
+  private int drone_id;
+  private String drone_cmd;
+
+  public int getDrone_ID(){
+    return drone_id;
+  }
+
+  public String getDrone_cmd(){
+    return drone_cmd;
+  }
+
+  public void set_cmd(String cmd){
+    this.drone_cmd = cmd;
+  }
+}
--- src/Product.java
+++ src/Product.java
@@ -1,15 +1,17 @@
-private int product_id;
-private int product_weight;
+class Product{
+  private int product_id;
+  private int product_weight;
 
-public Product(int product_id, int product_weight){
+  public Product(int product_id, int product_weight){
     this.product_id;
     this.product_weight;
-}
+  }
 
-public int getProduct_id(){
+  public int getProduct_id(){
     return product_id;
-}
+  }
 
-public Int getProduct_weight(){
+  public Int getProduct_weight(){
     return product_weight;
+  }
 }
