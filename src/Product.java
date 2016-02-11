ea4eca9ac3099e0e1d176c9d39992ef30a65fd36
Perelan <sinriat@gmal.com>
2016-02-11 19:11:15 +0100

    Fixed

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,15 @@
-
-private String product_id;
+private int product_id;
 private int product_weight;
+
+public Product(int product_id, int product_weight){
+    this.product_id;
+    this.product_weight;
+}
+
+public int getProduct_id(){
+    return product_id;
+}
+
+public Int getProduct_weight(){
+    return product_weight;
+}
