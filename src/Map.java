2830a50b710a3499e59595955fc6a741cc5f9e71
Sharu95 <sharanak@uio.no>
2016-02-11 20:40:50 +0100

    Fixed Drone and Map

--- src/Map.java
+++ src/Map.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class Map
 {
     private final int GRID_SIZE = 10000;
 
     private int[][] grid;
-    private int nrOfDrone;
+    private int nrOfDrones;
     private int droneMaxLoad;
     private int deadLine;
     private ArrayList<Drone> drone_list;
